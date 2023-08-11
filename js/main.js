72583234b92c723a7640f28bb8e5526cdd270065
daria <egor.kachur@gmail.com>
2023-08-12 00:05:20 +0300

    img/ordering__bg.jpg #	new file:   img/ordering__col1-tel.png
    :wq
    :q!

--- js/main.js
+++ js/main.js
@@ -8,49 +8,16 @@ burger.addEventListener('click', function(){
 
 });
 
-var clicked = ''; //переменная для сохранения закрывающегося элемента
-document.addEventListener('click', e => { //можно вешать и не на документ
-	const targetId = e.target.getAttribute('data-target') || '' // достаем id элемента
-	if(!targetId) return // если его нет, выходим из скрипта
-	const target = document.getElementById(targetId) // ссылка на сам элемент
-	if(clicked && clicked !== target) clicked.classList.remove('open') 
-	//если это не первое открытие и не 2ой раз открывается текущий, то скрываем
-	target.classList.add('open');
-	clicked = target //сохраняем ссылку в замыкании
+/*filter */
+
+let filter__btn = document.querySelector('.filter-button');
+let filter = document.querySelector('.catalog__wrapper');
+let close = document.querySelector('.filter__close');
+filter__btn.addEventListener('click', function(){
+	filter.classList.add('active');
+	//filter.classList.toggle('active');
 });
-
-
-  // Modal
-  const modalTrigger = document.querySelectorAll('[data-modal]'),
-  modal = document.querySelector('.modal'),
-  modalCloseBtn = document.querySelector('[data-close]');
-modalTrigger.forEach(btn => {
-  btn.addEventListener('click', function() {
-	  modal.classList.add('show');
-	  modal.classList.remove('hide');
-	  // Либо вариант с toggle - но тогда назначить класс в верстке
-	  //document.body.style.overflow = 'hidden';
-
-  });
-});
-
-function closeModal() {
-  modal.classList.add('hide');
-  modal.classList.remove('show');
-  // Либо вариант с toggle - но тогда назначить класс в верстке
-  //document.body.style.overflow = '';
-}
-
-modalCloseBtn.addEventListener('click', closeModal);
-
-modal.addEventListener('click', (e) => {
-  if (e.target === modal) {
-	  closeModal();
-  }
-});
-
-document.addEventListener('keydown', (e) => {
-  if (e.code === "Escape" && modal.classList.contains('show')) { 
-	  closeModal();
-  }
+close.addEventListener('click', function(){
+	filter.classList.remove('active');
+	//filter.classList.toggle('active');
 });
